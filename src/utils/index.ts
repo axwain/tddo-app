78df4b61ab91d12790692de2ee22c124b7c21c02
AXwain <5733365+axwain@users.noreply.github.com>
2023-03-20 12:34:13 -0500

    Implemented utils functions to update Todo list

---
+++ src/utils/index.ts
@@ -0,0 +1,24 @@
+import { Todo } from '../types';
+
+export const findTodoItemIndex = (items: Todo[], searchTerm: string) => {
+  return items.findIndex((item) => item.description === searchTerm);
+};
+
+export const addTodoItem = (items: Todo[], itemToAdd: Todo) => {
+  if (findTodoItemIndex(items, itemToAdd.description) < 0) {
+    return [...items, itemToAdd];
+  }
+
+  return items;
+};
+
+export const updateTodoList = (items: Todo[], itemToUpdate: Todo) => {
+  const index = findTodoItemIndex(items, itemToUpdate.description);
+  if (index < 0) {
+    return items;
+  }
+
+  const newList = [...items];
+  newList.splice(index, 1, itemToUpdate);
+  return newList;
+};
