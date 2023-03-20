a44a99918987570e44c0fb4fa0cc87b13c74ee7b
AXwain <5733365+axwain@users.noreply.github.com>
2023-03-19 22:24:47 -0500

    Replaced SolidJs with ReactJs because SolidJs tests were too slow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [react()],
   server: {
     port: 3000,
   },
