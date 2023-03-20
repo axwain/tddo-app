587ec6774ea742928d7046418c8b716a28b723dc
AXwain <5733365+axwain@users.noreply.github.com>
2023-03-20 18:36:23 -0500

    Implemented loading and saving of todo items list

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.css';
 import { ErrorMessage } from './components/ErrorMessage';
 import { ItemList } from './components/ItemList';
 import { NewTodoItem } from './components/NewTodoItem';
+import { loadItems, saveItems, setupAppFolder } from './storage';
 import { Todo } from './types';
 import { addTodoItem, removeCompletedItems, updateTodoList } from './utils';
 
 const App: React.FC = () => {
   const [error, setError] = useState(false);
   const [items, setItems] = useState<Todo[]>([]);
+  const [appDir, setAppDir] = useState('');
+
+  const save = (otherList: Todo[]) => {
+    console.log('starting save', appDir);
+    if (otherList !== items) {
+      saveItems(otherList, appDir).then(() => console.log('Items Saved'));
+    }
+  };
 
   const handleAddItem = (description: string) => {
     const newItem: Todo = { description, isCompleted: false };
     const newList = addTodoItem(items, newItem);
     setItems(newList);
     setError(newList.length === items.length);
+    save(newList);
   };
 
   const handleUpdate = (item: Todo) => {
-    setItems(updateTodoList(items, item));
+    const newList = updateTodoList(items, item);
+    setItems(newList);
     setError(false);
+    save(newList);
   };
 
   const handleClearComplete = () => {
-    setItems(removeCompletedItems(items));
+    const newList = removeCompletedItems(items);
+    setItems(newList);
     setError(false);
+    save(newList);
   };
+
+  useEffect(() => {
+    console.log('Setting app folder');
+    setupAppFolder()
+      .then(async (appFolder: string) => {
+        if (appFolder) {
+          console.log('appFolder obtained', appFolder);
+          const loadedItems = await loadItems(appFolder);
+          setAppDir(appFolder);
+          setItems(loadedItems);
+        }
+      })
+      .then(() => console.log('finished loading items'));
+  }, []);
 
   return (
     <>
