b2161122959d0db985dd1206e8d036cf5cab7f59
AXwain <5733365+axwain@users.noreply.github.com>
2023-03-19 23:00:46 -0500

    Applied Prettier formatting to source code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { describe, expect, it } from '@jest/globals';
 import { render } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
 describe('App - sample test', () => {
   it('should render', () => {
     const { queryByText } = render(<App />);
     expect(queryByText('React')).toBeDefined();
-  })
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-
-import logo from './logo.svg';
 import styles from './App.module.css';
+import logo from './logo.svg';
 
 const App: React.FC = () => {
   return (
