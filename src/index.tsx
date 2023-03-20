3bfabf1387ba8fe445a25aef8ee7c50d2b424792
Johnny Lamus <5733365+axwain@users.noreply.github.com>
2023-03-20 18:38:52 -0500

    Impl storage persistence (#8)
    * Installed neutralinojs lib
    
    * Deleted unneeded tests
    
    * Implemented saving and loading functions
    
    * Set up neutralinojs native API
    
    * Implemented loading and saving of todo items list

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 /* @refresh reload */
+import { init } from '@neutralinojs/lib';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+init();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
