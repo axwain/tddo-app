6469beb3314036797e633ef2d673b3b6ce919224
axwain <5733365+axwain@users.noreply.github.com>
2024-05-21 20:41:51 -0500

    refactor: started removing neutralino
    Signed-off-by: axwain <5733365+axwain@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,7 @@
-import { app, events, init, window } from '@neutralinojs/lib';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-const onWindowClose = () => {
-  app.exit();
-};
-
-const initCustomWindowBar = async () => {
-  const closeBtn = document.getElementById('closeBtn');
-
-  closeBtn?.addEventListener('click', onWindowClose);
-
-  await window.setDraggableRegion('draggableRegion');
-};
-
-init();
-events.on('windowClose', onWindowClose);
-events.on('ready', initCustomWindowBar);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   },
   base: '',
   build: {
-    outDir: 'client/resource',
     target: 'esnext',
   },
 });
