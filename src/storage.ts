8cf9af18e14a698e13ce4d936a246cd0d04c5e4c
axwain <5733365+axwain@users.noreply.github.com>
2024-05-21 21:30:09 -0500

    refactor: removed neutralino remains and adjusted css global module
    Signed-off-by: axwain <5733365+axwain@users.noreply.github.com>

--- src/storage.ts
+++ src/storage.ts
@@ -1,8 +1,7 @@
-import { filesystem, os } from '@neutralinojs/lib';
 import { Todo } from './types';
 
 const getUserFolder = async () => {
-  return (await os.getEnv('userprofile')) || (await os.getEnv('HOME')) || '';
+  return './';
 };
 
 const getAppFolder = (homedir: string) => {
@@ -13,36 +12,14 @@ export const setupAppFolder = async () => {
   const userFolder = await getUserFolder();
   const appFolder = getAppFolder(userFolder);
   if (userFolder) {
-    try {
-      console.log('finding app folder', appFolder);
-      await filesystem.readDirectory(appFolder);
-      console.log('return app folder', appFolder);
-      return appFolder;
-    } catch (e) {
-      if (e.code === 'NE_FS_NOPATHE') {
-        console.log('creating', appFolder);
-        await filesystem.createDirectory(appFolder);
-        console.log('return app folder', appFolder);
-        return appFolder;
-      }
-    }
+    // if appFolder doesn't exist, create it
   }
 
-  console.log('no app folder out');
   return '';
 };
 
 const getItemsFilePath = (appFolder: string) => `${appFolder}/items.json`;
 const getItemsBackupPath = (appFolder: string) => `${appFolder}/items.json.bk`;
-
-const itemsFileExist = async (itemsPath: string) => {
-  try {
-    await filesystem.readDirectory(itemsPath);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
 
 export const saveItems = async (
   list: Todo[],
@@ -53,22 +30,16 @@ export const saveItems = async (
     console.log('appFolder is set up', appFolder);
     const itemsFilePath = getItemsFilePath(appFolder);
     const itemsBackupPath = getItemsBackupPath(appFolder);
-    if (await itemsFileExist(itemsFilePath)) {
-      console.log('Backing up items', itemsBackupPath);
-      await filesystem.copyFile(itemsFilePath, itemsBackupPath);
-    }
+    // if save file exists, save backup
 
     console.log('Saving items up', itemsFilePath);
-    await filesystem.writeFile(itemsFilePath, JSON.stringify(list, null, 2));
+    // save file
   }
 };
 
 export const loadItems = async (appFolder: string): Promise<Todo[]> => {
   const itemsFilePath = getItemsFilePath(appFolder);
-  if (await itemsFileExist(itemsFilePath)) {
-    const itemsRawData = await filesystem.readFile(itemsFilePath);
-    return JSON.parse(itemsRawData);
-  }
+  // if save file exists, load it
 
   return [];
 };
