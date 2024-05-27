94bd75253a20c2caccd0a79d3b358d3852655bf2
axwain <5733365+axwain@users.noreply.github.com>
2024-05-26 23:04:39 -0500

    build: adjusted icon generation scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,11 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
-}
+    '@electron-toolkit/eslint-config-prettier',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-uses-react': 'error',
+  },
+};
